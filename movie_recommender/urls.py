20b8e08decd3edbcc3b2264bd5dc82b923784ee1
Shampad Sharkar <sompodsign@gmail.com>
2021-02-02 22:27:04 +0600

    Initial commit

---
+++ movie_recommender/urls.py
@@ -0,0 +1,11 @@
+
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('movie.urls', namespace='movie')),
+    path('account/', include('account.urls', namespace='account')),
+    path('account/', include('django.contrib.auth.urls')),
+
+]
---
+++ todos.py
@@ -0,0 +1,1 @@
+# todo 1: fix trash can icon and edit icon in library page.
